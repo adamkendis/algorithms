ffb8aef7bc878652c6224047f5b89834924ab1ac
Adam Kendis <atkendis@gmail.com>
2019-10-18 23:35:22 -0700

    angryProfessor completed.

---
+++ angryProfessor.js
@@ -0,0 +1,13 @@
+function angryProfessor(k, a) {
+    let onTimeCount = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] <= 0) {
+            onTimeCount++;
+        }
+        if (onTimeCount >= k) {
+            return 'NO';
+        }
+    }
+    return 'YES';
+}
